4e21fdf22be9d1403f43fc16de8baf8d8faf8377
COLD-PLAY <mixishizi@163.com>
2017-08-11 17:09:33 +0800

    add start.sh

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -v
+# test.sh
+
+# echo 'Hello, World'
+i=0;
+# ((i++))
+# echo $i
+
+# let i++
+# echo $i
+
+# expr $i + 1
+
+# echo $i
+
+# type type
+# type let
+# type expr
+# type bc
+# type awk
+
+# echo "hello world"
